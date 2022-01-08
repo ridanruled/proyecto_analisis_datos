e99a3366ec2ea57d730a9401b9d30128868fda66
Ricardo Ruiz <ricardo.ruled@gmail.com>
2022-01-07 18:23:23 -0600

    Se agregan comentarios de funcionalida.

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import csv
-import seaborn as sns
 import matplotlib.pyplot as plt
 from matplotlib.ticker import PercentFormatter 
 
-
+# Función para el menú principal
 def menu():
+    # Solicita la entrada de una opción para el menu
     try:
         opcion = int(input("""
         Menu principal.
@@ -22,6 +22,7 @@ def menu():
         print("Entrada no válida")
         main()
 
+# Función que imprime el encabezado del nombre de la empresa.
 def header():
     print("""
 |||| ||||  ||   || ||||| |||||| |||||| ||    ||    ||   |||||| |||||| ||    || |||| |||| |||| |||| ||||     |||||||||||||
@@ -31,12 +32,16 @@ def header():
 |||| ||||  ||   || ||||| ||  || ||||||    ||       |||| |||||| ||||||    ||    ||||  ||  |||| |||| ||||      (__)           (__)
 """)
 
+# Función que determina las rutas de exportación e importación más demandadas que recibe la ruta del archivo.
 def rutas_mas_demandadas(path):
     contador_rutas_exportacion = {}
     contador_rutas_importacion = {}
 
+    # Abre el archivo csv en mode lectura.
     with open(path,'r') as archivo:
+        # Crea el objeto lector que transforma las filas del csv en diccionarios.
         lector = csv.DictReader(archivo)
+        #Permite contar la frecuencia de cada ruta a través de un diccionario y controles de flujo.
         for fila in lector:
             if(fila['direction'] == 'Exports'):
                 clave = fila['origin'] + '-' + fila['destination']
@@ -51,19 +56,27 @@ def rutas_mas_demandadas(path):
                 else:
                     contador_rutas_importacion[clave] = 1
 
-
+    # Ordena de mayor a menor las rutas con su frecuencia
     contador_rutas_exportacion_ordenado = sorted(contador_rutas_exportacion.items(), key = lambda tupla: tupla[1], reverse= True)
     contador_rutas_importacion_ordenado = sorted(contador_rutas_importacion.items(), key = lambda tupla: tupla[1], reverse= True)
+    
+    # Selecciona las 10 primeras rutas.
     diez_rutas_mas_demandadas_exportacion = contador_rutas_exportacion_ordenado[:10]
     diez_rutas_mas_demandadas_importacion = contador_rutas_importacion_ordenado[:10]
 
+    # Regresa las 10 rutas más demandas de exportación y las 10 rutas más demandas de importación.
     return diez_rutas_mas_demandadas_exportacion,diez_rutas_mas_demandadas_importacion
 
+# Función que determina las rutas de exportación e importación que generan un mayor valor.
 def rutas_mas_total_valor(path):
+    # Diccionarios para guardar las rutas con sus valores.
     ruta_valor_total_exportaciones = {}
     ruta_valor_total_importaciones = {}
+
+    # Abre el archivo csv en mode lectura.
     with open(path,'r') as archivo:
         lector = csv.DictReader(archivo)
+        #Permite contar la frecuencia de cada ruta a través de un diccionario y controles de flujo.
         for fila in lector:
             if(fila['direction'] == 'Exports'):
                 clave = fila['origin'] + '-' + fila['destination']
@@ -77,8 +90,11 @@ def rutas_mas_total_valor(path):
                     ruta_valor_total_importaciones[clave].append(fila['total_value'])
                 else:
                      ruta_valor_total_importaciones[clave] = [fila['total_value']]
-
+    
+    # Creación de diccionario que guardará la suma de los valores de cada ruta.
     ruta_valor_suma_total_exportaciones = {}
+
+    # Ciclo for que permite iterar en cada elemento del diccionario para realizar la suma de los elementos de la lista en el valor de cada clave.
     for clave, lista in ruta_valor_total_exportaciones.items():
         contador = 0
         for elemento in lista:
@@ -91,14 +107,19 @@ def rutas_mas_total_valor(path):
             contador += int(elemento)
         ruta_valor_suma_total_importaciones[clave] = contador
     
+    # Ordena los diccionarios de mayor a menor de acuerdo con el valor.
     sorted_ruta_valor_suma_total_exportaciones = sorted(ruta_valor_suma_total_exportaciones.items(), key = lambda tupla: tupla[1], reverse= True)
     sorted_ruta_valor_suma_total_importaciones = sorted(ruta_valor_suma_total_importaciones.items(), key = lambda tupla: tupla[1], reverse= True)
 
+    # Regresa las 10 rutas de exportación y las 10 rutas de importación con mayor valor.
     return sorted_ruta_valor_suma_total_exportaciones[:10], sorted_ruta_valor_suma_total_importaciones[:10]
 
+# Función que determina el valor generado por cada medio de transporte
 def medios_transporte(path):
+    #Creación del diccionario que permite guardar cada medio de transporte con sus valores totales.
     medios_de_transporte_valores = {}
 
+    # Abre el archivo csv en mode lectura.
     with open(path,'r') as archivo:
         lector = csv.DictReader(archivo)
         for fila in lector:
@@ -107,6 +128,7 @@ def medios_transporte(path):
             else:
                 medios_de_transporte_valores[fila['transport_mode']] = [fila['total_value']]
 
+    # Creación de diccionario para guardar la suma de los valores de cada medio de transporte.
     medio_transporte_total = {}
     for clave, lista in medios_de_transporte_valores.items():
         contador = 0
@@ -115,15 +137,24 @@ def medios_transporte(path):
 
         medio_transporte_total[clave] = contador
 
+    # Ordena el diccionario de acuerdo con sus valores.
     sorted_medio_transporte_total = sorted(medio_transporte_total.items(), key = lambda tupla: tupla[1], reverse= True)
+    #Regresa los medios de transporte con sus valores.
     return sorted_medio_transporte_total
 
+# Función que permite determinar el valor total generado por país.
 def pais_valores_totales(path):
+    #Crea diccionario que guardará los paises y una lista de los valores de cada país.
+    paises_valores = {}
+    # variable que sirve para sumar el valor total de todas las exportaciones e importaciones.
+    suma_total_valores = 0
+
+    # Abre el archivo csv en mode lectura.
     with open(path, 'r') as archivo:
-        suma_total_valores = 0
-        paises_valores = {}
         lector = csv.DictReader(archivo)
+        #Permite contar el valor de cada país a través de un diccionario y controles de flujo.
         for fila in lector:
+            # Se determina el valor de un país si este lo exportó o si este lo importó, ya que es el encargado de los costos.
             if(fila['direction'] == 'Exports'):
                 suma_total_valores += int(fila['total_value'])
                 if(fila['origin'] in paises_valores):
@@ -137,7 +168,9 @@ def pais_valores_totales(path):
                 else:
                     paises_valores[fila['destination']] = [fila['total_value']]
         
+        #Diccionario para guardar la suma de todos los valores de cada país.
         valores_por_pais = {}
+        # Ciclo flor que permite sumar los valores de la lista de cada clave.
         for clave, lista in paises_valores.items():
             contador = 0
             for elemento in lista:
@@ -145,23 +178,29 @@ def pais_valores_totales(path):
 
             valores_por_pais[clave] = contador
         
+        # Ordena el diccionario de mayor a menor de acuerdo con el valor de cada clave.
         sorted_valores_por_pais = sorted(valores_por_pais.items(), key = lambda tupla: tupla[1], reverse=True)
         
+        # Regresa los valores de cada país y la suma de todos los valores.
         return sorted_valores_por_pais, suma_total_valores
 
 
-
+# Función principal donde se lleva a cabo el programa.
 def main():
+    # Llama la función de header para imprimir nombre y logo de la empresa.
     header()
+    # Llama la función menú para imprimir las opciones y que el usuario seleccione una opción.
     opcion = menu()
+    # Ifs que permiten realizar las diferentes opciones disponibles en el menú.
     if(opcion == 1):
+        # Llamado de la función que regresa las rutas más demandadas.
         rutas_mayor_demanda_exportacion, rutas_mayor_demanda_importacion = rutas_mas_demandadas('./synergy_logistics_database.csv')
 
         header()
         print("""
         Las diez rutas de exportación con mayor demanda.
         """)
-
+        # Impresión de las rutas más demandadas.
         for ruta in rutas_mayor_demanda_exportacion:
             print(f'Ruta: {ruta[0]}, Frecuencia: {ruta[1]}')
         
@@ -170,13 +209,22 @@ def main():
         """)
         for ruta in rutas_mayor_demanda_importacion:
             print(f'Ruta: {ruta[0]}, Frecuencia: {ruta[1]}')
-        
+        #Creación de la gráfica de barras de las rutas más demandadas.
+        #Ajusta el tamaño de la letra de los titulos y etiquetas de los gráficos.
         plt.rcParams.update({'font.size': 8})
+
+        # Permite generar un espacio de 2 elementos para colocar una gráfica en cada uno.
         fig, ax = plt.subplots(2)
+
+        # Creación del gráfico de barras.
         ax[0].bar([pais for pais,frecuencia in rutas_mayor_demanda_exportacion], [frecuencia for pais,frecuencia in rutas_mayor_demanda_exportacion], color = "blue")
+        # Ángulo de las etiquetas del eje x.
         ax[0].tick_params(labelrotation=90)
+        # Titulo del gráfico
         ax[0].set_title("Las 10 rutas de exportación más demandadas (del año 2015 al año 2020)")
+        #etiqueta eje x
         ax[0].set_xlabel("Rutas")
+        # etiqueta eje y
         ax[0].set_ylabel("Frecuencia")
         ax[1].bar([pais for pais,frecuencia in rutas_mayor_demanda_importacion], [frecuencia for pais,frecuencia in rutas_mayor_demanda_importacion], color = "orange")
         ax[1].set_title("Las 10 rutas de importación más demandadas (del año 2015 al año 2020)")
@@ -186,6 +234,7 @@ def main():
         plt.subplots_adjust(hspace = 2)
         plt.show()
 
+        # Impresión de las rutas con mayor valor.
         rutas_exportacion_mayor_valor, rutas_importacion_mayor_valor = rutas_mas_total_valor('./synergy_logistics_database.csv')
         header()
         print("""
@@ -195,7 +244,7 @@ def main():
         for ruta in rutas_exportacion_mayor_valor:
             print(f'Ruta: {ruta[0]}, Valor: ${ruta[1]}')
     
-
+        # Creación de las gráficas de barras de las rutas con mayor valor.
         fig, ax_2 = plt.subplots(2)
         ax_2[0].bar([pais for pais,valor in rutas_exportacion_mayor_valor], [valor for pais,valor in rutas_exportacion_mayor_valor], color = "blue")
         ax_2[0].tick_params(labelrotation=90)
@@ -219,14 +268,19 @@ def main():
         plt.show()
 
     if(opcion == 2):
+        # Llamado de la función que regresa los medios de transporte y sus importes totales
         medios_transporte_por_valores_totales = medios_transporte('./synergy_logistics_database.csv')
+        # Llamado de la función para la impresión del nombre y logo de la empresa.
         header()
+
+        # Impresión de los medios de transporte y su importe total.
         print("""
         Importe total por medios de transporte (del año 2015 al año 2020).
         """)
         for medio in medios_transporte_por_valores_totales:
             print(f'Medio de transporte: {medio[0]}, importe total: ${medio[1]}')
 
+        # Creación de gráfico de barras de los medios de transporte y su importe total.
         plt.bar([medio for medio,valor in medios_transporte_por_valores_totales], [valor for medio,valor in medios_transporte_por_valores_totales] )
         plt.title("Importe total por medios de transporte (del año 2015 al año 2020).")
         plt.xlabel("Medio de transporte")
@@ -235,11 +289,16 @@ def main():
     
     if(opcion == 3):
 
+        # Llamado de la función que regresa los valores totales por país.
         valores_paises, suma_total_valores = pais_valores_totales('./synergy_logistics_database.csv')
         
+
         proporcion = 0
         paises_80_porciento = []
         ojiva = []
+        # Permite identificar los países que generan el 80% de exportaciones e importaciones.
+        # Crea la ojiva porcentual para el diagrama de Pareto.
+
         for pais, valor in valores_paises:
             if(proporcion < 80):
                 paises_80_porciento.append(pais)
@@ -249,12 +308,12 @@ def main():
             else:
                 ojiva.append(ojiva[-1] + ((valor / suma_total_valores) * 100))
                  
-                
-        print(ojiva)
-        print(suma_total_valores)
-        print(paises_80_porciento)
-
+        # Llamado de la función que imprime nombre y logo de la empresa
         header()
+
+        # Imprime los valores generados por cada país y la lista de los países que generaron el 80% de los valores
+        # exportación e importación.
+
         print("""
         Valores generados por pais.
         """)
@@ -269,6 +328,7 @@ def main():
         for i, pais in enumerate(paises_80_porciento):
             print(f'{i+1}. {pais}')
 
+        # Crea el diagrama de Pareto.
         fig, ax = plt.subplots()
         ax.bar([pais for pais,valor in valores_paises], [valor for pais,valor in valores_paises])
         ax.tick_params(labelrotation=90)
